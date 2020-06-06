bfa6be1af1d172507040d5c343c97a5dd0c11f68
Juhyeoklee <gur6531@naver.com>
2020-06-07 05:57:01 +0900

    add asset and result view

---
+++ DdoDdo-iOS/DdoDdo-iOS/Source/VCs/MatchingResultVC.swift
@@ -0,0 +1,26 @@
+//
+//  MatchingResultVC.swift
+//  DdoDdo-iOS
+//
+//  Created by 이주혁 on 2020/06/07.
+//  Copyright © 2020 이주혁. All rights reserved.
+//
+
+import UIKit
+
+class MatchingResultVC: UIViewController {
+
+    @IBOutlet var favoriteCollectionView: UICollectionView!
+    @IBOutlet var okButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.okButton.layer.cornerRadius = 
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
